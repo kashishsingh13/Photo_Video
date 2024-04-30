a3fd706fdb81fced269002c3c0573eb8cf9070c8
kashishsingh13 <160053598+kashishsingh13@users.noreply.github.com>
2024-04-30 21:52:44 +0530

    Add files via upload

--- VideoFragment.kt
+++ VideoFragment.kt
@@ -6,14 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.photosapplication.Adapter.VideoAdapter
 import com.example.photosapplication.Model.Video
@@ -21,6 +27,7 @@ import com.example.photosapplication.databinding.FragmentPhotoBinding
 import com.example.photosapplication.databinding.FragmentVideoBinding
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.io.File
 
 
 class VideoFragment : Fragment() {
@@ -41,7 +48,9 @@ class VideoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
          videoList.clear()
-        videoAdapter = VideoAdapter(requireContext(), videoList)
+        videoList.clear()
+        videoAdapter = VideoAdapter(requireContext(), videoList, ::onShareVideoClicked)
+//        videoAdapter = VideoAdapter(requireContext(), videoList)
         binding.recycle.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = videoAdapter
@@ -51,8 +60,11 @@ class VideoFragment : Fragment() {
             loadVideos()
         } else {
             requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+//
         }
+
     }
+
 
 
     private fun hasPermission(): Boolean {
@@ -77,7 +89,9 @@ class VideoFragment : Fragment() {
 
     private fun loadVideos() {
         videoList.clear()
+
         val cursor = requireContext().contentResolver.query(
+
                 MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
                 null,
                 null,
@@ -105,8 +119,30 @@ class VideoFragment : Fragment() {
 
 
     }
+    private fun onShareVideoClicked(video: Video) {
+        val fileUri = FileProvider.getUriForFile(
+            requireContext(),
+            "${requireContext().packageName}.provider",
+            File(video.path)
+        )
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, fileUri)
+            type = "video/*"
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+        try {
+            startActivity(Intent.createChooser(shareIntent, "Share Video"))
+        } catch (e: Exception) {
+            Toast.makeText(requireContext(), "No app found to handle this action", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+
 
     companion object {
-        private const val PERMISSIONS_REQUEST_CODE = 121
+        const val PERMISSIONS_REQUEST_CODE = 123
     }
 }
